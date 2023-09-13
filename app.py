5233ae530a82bd7f7f3384998c6e457e317e6058
cao toan khoa <caotoankhoa5@gmail.com>
2023-09-13 22:22:02 +0700

    init commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World from Flask in Docker asfasfasf!'
+    return 'Hello, World from Flask in Docker asfasfasfầ!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)  # Listen on all available network interfaces
